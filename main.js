ae2539e46b3304e13f43886e1ef363366c3cda44
zelbaz946 <ziyad.elbaz@me.com>
2021-04-07 12:02:04 -0500

    initial set up

---
+++ main.js
@@ -0,0 +1,15 @@
+module.exports = (instance, properties, context) => {
+	//load our data
+	let finds = properties.find_list.split(",");
+	let replaces = properties.replace_list.split(",");
+	let text = properties.text;
+
+	//iterate over both arrays
+	for (let index in finds) {
+		let elemFind = finds[index];
+		let elemReplace = replaces[index];
+		text = text.replaceAll(elemFind, elemReplace);
+	}
+
+	instance.publishState("text_processed", text);
+};
