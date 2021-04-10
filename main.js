abcfc4fefd24489c8d960e21ca303befb6b3f9a7
zelbaz946 <ziyad.elbaz@me.com>
2021-04-10 16:29:45 -0500

    Updated algo

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
 module.exports = (instance, properties, context) => {
-	// Error Handling
-	if (typeof properties.find_list !== "string") {
-		throw new Error("Find list must be a text input");
+  // Error Handling
+  if (typeof properties.find_list !== 'string') {
+    throw new Error('Find list must be a text input');
+  }
+  if (typeof properties.replace_list !== 'string') {
+    throw new Error('Replace list must be a text input');
+  }
+  if (typeof properties.text !== 'string') {
+    throw new Error('Text must be a text input');
 	}
-	if (typeof properties.replace_list !== "string") {
-		throw new Error("Replace list must be a text input");
+	
+
+  // load our data
+  const finds = properties.find_list.split(',');
+	const replaces = properties.replace_list.split(',');
+
+	if (finds.length !== replaces.length) {
+		throw new Error('Finds & Replaces must contain the same number of arguments');
 	}
-	if (typeof properties.text !== "string") {
-		throw new Error("Text must be a text input");
+	const { text } = properties;
+	const textArr = text.split(' ');
+
+  // iterate over both arrays
+	for (let i = 0; i < textArr.length; i += 1) {
+		const word = textArr[i];
+		for (let j = 0; j < finds.length; j += 1) {
+			const find = finds[j];
+			const replace = replaces[j];
+			if (word === find) {
+				textArr[j] = replace;
+			}
+		}
 	}
 
-	// load our data
-	const finds = properties.find_list.split(",");
-	const replaces = properties.replace_list.split(",");
-	let text = properties.text;
-
-	// iterate over both arrays
-	for (const index in finds) {
-		const elemFind = finds[index];
-		const elemReplace = replaces[index];
-		text = text.replaceAll(elemFind, elemReplace);
-	}
-
-	instance.publishState("text_processed", text);
+  instance.publishState('text_processed', textArr.join(' '));
 };
-
-// iterate over the array of inputs
-// iterate over the text
